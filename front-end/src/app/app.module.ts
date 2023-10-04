249b081b0347317aadda8e95413efca3f4f4b582
Marius Andronescu <spider_child2001@yahoo.com>
2023-10-04 17:08:42 +0300

    added background color,simplified the navbr and added the components to the main component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,9 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { FoodDisplayComponent } from './components/food-display/food-display.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SearchBarComponent,
+    FoodDisplayComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
